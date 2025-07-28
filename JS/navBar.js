f617565ad97247d99a639910d6c11e4291ae5ebc
Zona2LaRomana <zonalaromana@gmail.com>
2025-07-27 21:38:35 -0400

    Update navBar.js
    Corregir dimensiones del iFrame

--- JS/navBar.js
+++ JS/navBar.js
@@ -46,7 +46,7 @@ if(Nombre){
                                              <div class="taller" id="${fila.codigoSeminario}">
                                              <div class="content-taller">
                                              <h3>${fila.tituloSeminario}</h3>
-                                             <iframe id="v-${fila.codigoSeminario}" width="500" height="300" src="${fila.frameYoutube}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                             <iframe id="v-${fila.codigoSeminario}" width="90%" height="300" src="${fila.frameYoutube}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                                              <p>${fila.descripcionYoutube}</p>
                                              <div class="asignaciones">
                                              <div class="asignacion">
